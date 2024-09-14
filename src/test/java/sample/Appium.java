6b228b34ea143ec148f0f0069e76a67be51b77c1
Lucky <lakshmikrishna.narra@gmail.com>
2024-09-14 11:53:12 +0530

    Lakshmi

---
+++ src/test/java/sample/Appium.java
@@ -0,0 +1,17 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Appium {
+  @Test
+  public void appium() {
+	  System.out.println("mobile automation");
+	 
+	 
+  }
+  
+  @Test
+  public void mtest() {
+	  System.out.println("mobile manual testing");
+  }
+}
---
+++ src/test/java/sample/selenium.java
@@ -0,0 +1,14 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class selenium {
+  @Test
+  public void testng() {
+		System.out.println("TestNG Project");
+	}
+  @Test
+  public void cucumber() {
+		System.out.println("cucumber project");
+	}
+}
